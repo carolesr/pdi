71ed81441b72b13bba843aacca48af745ab32c2c
carol_rosa <caroline.rosa.silva@gmail.com>
2021-07-18 19:56:22 -0300

    bloom lighting box blur

---
+++ pdi/Trab3/main.py
@@ -0,0 +1,76 @@
+#===============================================================================
+# Exemplo: segmentação de uma imagem em escala de cinza.
+#-------------------------------------------------------------------------------
+# Autor: Bogdan T. Nassu
+# Universidade Tecnológica Federal do Paraná
+#===============================================================================
+
+#===============================================================================
+# Alunos: Caroline Rosa & Leonardo Trevisan
+#===============================================================================
+
+from os import altsep
+import sys
+import timeit
+import numpy as np
+import cv2
+import sys
+
+#===============================================================================
+
+INPUT_IMAGE =  'jogo.bmp'
+THRESHOLD = 0.5
+ALPHA = 1
+BETA= 0.1
+    
+def bright_pass(img):
+    img_out = img.copy()
+    for row in range(img.shape[0]):
+        for col in range(img.shape[1]):
+            img_out[row][col] = img[row][col] if img[row][col][1] > THRESHOLD else 0
+
+    return img_out
+
+def bloom_box_blur(img, original):
+    blur = np.zeros((img.shape[0], img.shape[1], img.shape[2]))
+    # Borra 4 vezes, com janelas de 16, 25, 36 e 49
+    for round in range(4,8):
+        blur += cv2.blur(img, (round*round, round*round))
+
+    # Não achei como ser mt criativa aqui, principalmente com o Alpha, não vejo utilidade pra ele pq só estoura a imagem
+    # Pra deixar um resultado melhor tinha q tratar contraste e aquelas parada porém sem saco
+    return original*ALPHA + blur*BETA
+
+
+#===============================================================================
+
+def main ():
+    img = cv2.imread (INPUT_IMAGE)
+    if img is None:
+        print ('Erro abrindo a imagem.\n')
+        sys.exit ()
+
+    # É uma boa prática manter o shape com 3 valores, independente da imagem ser
+    # colorida ou não. Também já convertemos para float32.
+    img = img.reshape ((img.shape [0], img.shape [1], img.shape [2]))
+    img = img.astype (np.float32) / 255
+
+    # Converte imagem RGB para HSL para fazer o bright-pass
+    imgHLS = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    bright_pass_hsl = bright_pass(imgHLS)
+    # Converte de volta pra RGB
+    bright_pass_img = cv2.cvtColor(bright_pass_hsl, cv2.COLOR_HLS2BGR)
+    cv2.imwrite (f'bright_pass_img.png', bright_pass_img*255)
+    print('Bright Pass done')
+
+    bloom_img = bloom_box_blur(bright_pass_img, img)
+    cv2.imwrite (f'bloom-box-blur.png', bloom_img*255)
+    print('Box-Blur Bloom done')
+
+    cv2.waitKey ()
+    cv2.destroyAllWindows ()
+
+if __name__ == '__main__':
+    main ()
+
+#===============================================================================
