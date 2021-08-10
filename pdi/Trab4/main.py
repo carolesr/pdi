d80bb5072a75d891d66800efd18bcead5f79e383
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2021-08-10 02:45:22 -0300

    Ajustes buscando tratar melhor superblobs
    -Inunda agora é um algoritmo iterativo ao invés de recursivo
    -Limiarização Adaptativa usa uma regra diferente para testar pixel
    -Parâmetros da Limiarização Adaptativa foram ajustados
    -Agora, processo de inferir quantidades de arroz em superblob é iterativo, evitando problemas em que o tamanho de superblobls afeta a média e impede inferência estatística pelo tamanho.

--- pdi/Trab4/main.py
+++ pdi/Trab4/main.py
@@ -13,13 +13,12 @@ import timeit
 
 #===============================================================================
 
-INPUT_IMAGE =  '60.bmp'
-ALTURA = 300
-LARGURA = 300
-FATOR = 1.3
+ALTURA = 150
+LARGURA = 150
+FATOR = 0.225
 ALTURA_MIN = 1
 LARGURA_MIN = 1
-N_PIXELS_MIN = 20
+N_PIXELS_MIN = 30
     
 def integral(img):
     img_aux = np.zeros((img.shape[0], img.shape[1]))
@@ -38,52 +37,45 @@ def integral(img):
     return img_aux
 
 def limiarizacao_adaptativa(img):
-    
     img_aux = integral(img)
-
     alto2 = ALTURA // 2
     laro2 = LARGURA // 2
     for row in range(img_aux.shape[0]):
         for col in range(img_aux.shape[1]):
-                x1 = col - laro2 - 1
-                x2 = col + laro2
-                y1 = row - alto2 - 1
-                y2 = row + alto2
-                if (x1 < 0):
-                    x1 = 0
-                if (y1 < 0):
-                    y1 = 0
-                if (x2 >= img.shape[1]):
-                    x2 = img.shape[1] - 1
-                if (y2 >= img.shape[0]):
-                    y2 = img.shape[0] - 1
-
-                valor = img_aux[y2][x2] - img_aux[y1][x2] - img_aux[y2][x1] + img_aux[y1][x1]
-                area = float((x2 - x1) * (y2 - y1))
-                media = float(valor) / area
-                img[row][col] = 1.0 if img[row][col] > (media*FATOR) else 0.0
+            x1 = col - laro2 - 1
+            x2 = col + laro2
+            y1 = row - alto2 - 1
+            y2 = row + alto2
+            if (x1 < 0):
+                x1 = 0
+            if (y1 < 0):
+                y1 = 0
+            if (x2 >= img.shape[1]):
+                x2 = img.shape[1] - 1
+            if (y2 >= img.shape[0]):
+                y2 = img.shape[0] - 1
+            valor = img_aux[y2][x2] - img_aux[y1][x2] - img_aux[y2][x1] + img_aux[y1][x1]
+            area = float((x2 - x1) * (y2 - y1))
+            media = float(valor) / area
+            img[row][col] = 1.0 if img[row][col] - media > FATOR else 0.0
 
     return img
 
 def check_bordas(img, row, col):
     return row < img.shape[0] and row >= 0 and col < img.shape[1] and col >= 0
 
-
 def rotula (img, largura_min, altura_min, n_pixels_min):
     result = []
     label = 2.0
     
     for row in range(img.shape[0]):
-        for col in range(img.shape[1]):
-                        
+        for col in range(img.shape[1]):          
             if img[row][col] == 1:                
                 blob = {
                     'label': label,
                     'pixels': []
                 }
-                
                 blob, img = inunda(blob, img, row, col)
-            
                 blob = {
                     'label': label,
                     'n_pixels': len(blob['pixels']),
@@ -94,54 +86,59 @@ def rotula (img, largura_min, altura_min, n_pixels_min):
                 }
                 if blob['n_pixels'] >= n_pixels_min and blob['B'] - blob['T'] >= altura_min and blob['R'] - blob['L'] >= largura_min:
                     result.append(blob)
-                
-                label += 1.0
-                
+                label += 1.0         
     return result
-    
 
 def inunda (blob, img, row, col):
-            
     label = blob['label']
-    img[row][col] = label
-    blob['pixels'].append({ 'row': row, 'col': col })
-    # print({ 'label': label, 'row': row, 'col': col })
-                
-    if check_inunda(img, row, col-1):
-        blob, img = inunda(blob, img, row, col-1)
-        
-    if check_inunda(img, row+1, col):
-        blob, img = inunda(blob, img, row+1, col)
-        
-    if check_inunda(img, row-1, col):
-        blob, img = inunda(blob, img, row-1, col)
-
-    if check_inunda(img, row, col+1):
-        blob, img = inunda(blob, img, row, col+1)
-    
+    stack = []
+    stack.append((row, col))
+    while len(stack) > 0:
+        row, col = stack[0]
+        if img[row][col] != label:
+            blob['pixels'].append({ 'row' : row, 'col' : col })
+            img[row][col] = label
+            if check_inunda(img, row, col-1):
+                stack.append((row, col-1))
+            if check_inunda(img, row+1, col):
+                stack.append((row+1, col))
+            if check_inunda(img, row-1, col):
+                stack.append((row-1, col))
+            if check_inunda(img, row, col+1):
+                stack.append((row, col+1))
+        stack.pop(0)
     return blob, img
 
 def check_inunda(img, row, col):
-    return range_permitido(img, row, col) and img[row][col] == 1 and pixel_nao_mapeado(img, row, col)
-
-def pixel_nao_mapeado(img, row, col):
-    return img[row][col] == 1.0
+    return range_permitido(img, row, col) and img[row][col] == 1
 
 def range_permitido(img, row, col):
     return row >= 0 and row < img.shape[0] and col >= 0 and col < img.shape[1]
 
-
 def calcula_total_arroz(componentes):
+    superblobs = []
+    superblobscount = -1
 
-    tamanhos = [c['n_pixels'] for c in componentes]
-    media = np.mean(tamanhos)
-
-    total_arroz = 0
-    for blob in componentes:
-        tam = blob['n_pixels']
-        n = tam / media
-        # print({'tam': tam, 'n':n, 'round n': round(n)})
-        total_arroz += round(n)
+    while len(superblobs) != superblobscount:
+        tamanhos = [c['n_pixels'] for c in componentes]
+        media = np.mean(tamanhos)
+        superblobscount = len(superblobs)
+        i = 0
+        while i < len(componentes):
+            blob = componentes[i]
+            tam = blob['n_pixels']
+            n = tam / media
+            if round(n) >= 2.0:
+                superblobs.append(blob)
+                componentes.remove(blob)
+            else:
+                i += 1
+    
+    total_arroz = len(componentes)
+    print(superblobscount, "super componentes detectados.")
+    
+    for superblob in superblobs:
+        total_arroz += round(superblob['n_pixels'] / media)
 
     return total_arroz
 
@@ -151,14 +148,13 @@ def calcula_total_arroz(componentes):
 
 def main ():
     images = [
-        '60', 
-        '82', 
-        # '114', 
-        # '150', 
-        # '205'
+        '60',
+        '82',
+        '114',
+        '150',
+        '205'
         ]
     for input_image in images:
-
         img = cv2.imread (input_image + '.bmp')
         if img is None:
             print ('Erro abrindo a imagem.\n')
@@ -170,15 +166,15 @@ def main ():
         img = img.astype (np.float32) / 255
 
         gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        start_time = timeit.default_timer ()
+        start_time = timeit.default_timer()
         binarizada = limiarizacao_adaptativa(gray_scale)
         print ('Tempo: %f' % (timeit.default_timer () - start_time))
         cv2.imwrite (f'binarizada_{input_image}.png', binarizada*255)
         print(f'binarizada {input_image}.bmp done')
 
         start_time = timeit.default_timer ()
-        componentes = rotula (binarizada, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
-        n_componentes = len (componentes)
+        componentes = rotula(binarizada, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
+        n_componentes = len(componentes)
         print ('Tempo: %f' % (timeit.default_timer () - start_time))
         print ('%d componentes detectados.' % n_componentes)
 
