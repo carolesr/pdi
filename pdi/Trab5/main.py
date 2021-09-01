31c11005f97f406f44cb7208b166c5c561db0ea6
carol_rosa <caroline.rosa.silva@gmail.com>
2021-08-31 22:32:13 -0300

    normalizaçao

--- pdi/Trab5/main.py
+++ pdi/Trab5/main.py
@@ -9,28 +9,11 @@ import sys
 import numpy as np
 import cv2
 import sys
+import timeit
 
 #===============================================================================
 
 INPUT_IMAGE =  'img\\1.bmp'
-THRESHOLD = 180
-MINH = 110
-MAXH = 126
-MINL = 0.3
-MAXL = 0.6
-
-def grau_de_verde(img):
-
-    img_out = img.copy()
-    for row in range(img.shape[0]):
-        for col in range(img.shape[1]):
-            img_out[row][col] = 1 if eh_verde(img[row][col]) else img[row][col]
-
-    return img_out
-
-def eh_verde(pixel):
-    # print(pixel)
-    return pixel[0] > MINH and pixel[0] < MAXH and pixel[1] > MINL and pixel[1] < MAXL
 
 def graymask(img):
     img_out = img.copy()
@@ -40,13 +23,24 @@ def graymask(img):
             img_out[row][col] = (r * r + b * b + (1 - g) * (1 - g)) / 3
     return img_out
 
+def normalize(img):
+    maximo = img[..., 0].max()
+    minimo = img[..., 0].min()
+    print(maximo)
+    print(minimo)
+    img_out = img.copy()
+    for row in range(img.shape[0]):
+        for col in range(img.shape[1]):
+            img_out[row][col][0] = 1 - ((img[row][col][0] - minimo) / (maximo - minimo))
+    return img_out
+
 def apply(map, img, bg):
     img_out = img.copy()
     for row in range(img.shape[0]):
         for col in range(img.shape[1]):
             img_out[row][col][0] = (map[row][col][0] * img_out[row][col][0]) + ((1.0 - map[row][col][0]) * bg[row][col][0])
-            img_out[row][col][1] = (map[row][col][1] * img_out[row][col][1]) + ((1.0 - map[row][col][1]) * bg[row][col][1])
-            img_out[row][col][2] = (map[row][col][2] * img_out[row][col][2]) + ((1.0 - map[row][col][2]) * bg[row][col][2])
+            img_out[row][col][1] = (map[row][col][0] * img_out[row][col][1]) + ((1.0 - map[row][col][0]) * bg[row][col][1])
+            img_out[row][col][2] = (map[row][col][0] * img_out[row][col][2]) + ((1.0 - map[row][col][0]) * bg[row][col][2])
     return img_out
 
 #===============================================================================
@@ -66,9 +60,24 @@ def main ():
     bg = bg.reshape((bg.shape [0], bg.shape [1], bg.shape [2]))
     bg = bg.astype (np.float32) / 255
 
-    teste = graymask(img)
-    teste = apply(teste, img, bg)
-    cv2.imwrite (f'teste.png', teste * 255)
+    start_time = timeit.default_timer ()
+    mask = graymask(img)
+    print ('Tempo mask: %f' % (timeit.default_timer () - start_time))
+    cv2.imwrite (f'mask.png', mask * 255)
+
+    grayscale = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY)
+    grayscale = grayscale.reshape ((grayscale.shape [0], grayscale.shape [1], 1))
+    grayscale = grayscale.astype (np.float32) / 255
+
+    start_time = timeit.default_timer ()
+    normalizada = normalize(grayscale)
+    print ('Tempo normalize: %f' % (timeit.default_timer () - start_time))
+    cv2.imwrite (f'norm.png', normalizada * 255)
+
+    start_time = timeit.default_timer ()
+    result = apply(normalizada, img, bg)
+    print ('Tempo apply: %f' % (timeit.default_timer () - start_time))
+    cv2.imwrite (f'teste.png', result * 255)
     
 
     # # Converte imagem RGB para HSL
